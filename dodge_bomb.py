2f7e127961907d0c9061f2deab7344a2de515263
takeru <c0b23078ca@edu.teu.ac.jp>
2024-06-18 17:27:10 +0900

    演習１：追加機能１途中

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,12 +4,28 @@ import random
 import pygame as pg
 
 
-WIDTH, HEIGHT = 1300, 700 
+WIDTH, HEIGHT = 1000, 700 
 DELTA = {  # 移動量辞書
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, +5),
     pg.K_LEFT: (-5, 0), 
     pg.K_RIGHT: (+5, 0),
+    }
+
+def look():  # 飛ぶ方向
+    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
+    kk_img2 = pg.transform.flip(kk_img, True, False)
+    
+    return{
+    (-5, 0): kk_img,  # 0
+    (-5, +5): pg.transform.rotozoom(kk_img, 45, 2.0),  # 45
+    (0, +5): pg.transform.rotozoom(kk_img2, -90, 2.0),  # 90tf
+    (+5, +5): pg.transform.rotozoom(kk_img2, -50, 2.0),  # 45tf
+    (+5, 0): kk_img2,  # 0tf
+    (+5, -5): pg.transform.rotozoom(kk_img2, 50, 2.0),  # -45tf
+    (0, -5): pg.transform.rotozoom(kk_img2, 90, 2.0),  # -90ft
+    (-5, -5): pg.transform.rotozoom(kk_img, -45, 2.0),  # -45
+    (0, 0): kk_img
     }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -33,6 +49,7 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
+    kk_imgs = look()[(0, 0)]
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
     bomb_img = pg.Surface((20, 20))  # 一辺が20のSufaceを作る 
@@ -41,6 +58,7 @@ def main():
     bomb_rct = bomb_img.get_rect()
     bomb_rct.center  = random.randint(0, WIDTH), random.randint(0, HEIGHT) 
     vx, vy = +5, +5
+    
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -55,9 +73,11 @@ def main():
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
+        
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        kk_imgs = look()[tuple(sum_mv)]
         if kk_rct.colliderect(bomb_rct):  # 衝突判定
             return  # ゲームオーバー
         screen.blit(kk_img, kk_rct)
